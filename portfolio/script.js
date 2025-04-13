c7e6b10322234ab657689d742ff997b7ec4b5893
VIJAY <hosapetivijay18@gmail.com>
2025-04-14 00:03:36 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,7 @@
+// alert("THANK YOU FOR VISITING")
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
